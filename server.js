af5b22d123b2a9979f05ce0cccf157091cdd35d8
O M <150035512+procoderr45@users.noreply.github.com>
2026-01-31 16:37:50 +0530

    fixing port log

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require("./app.js");
+const PORT = 3000
 
-app.listen(3000, '0.0.0.0', () => {
-    console.log("Server listening on port 4000");
+app.listen(PORT, '0.0.0.0', () => {
+    console.log("Server listening on port", PORT);
 });
+
 
